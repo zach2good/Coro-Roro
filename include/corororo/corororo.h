e98543c3af4de648dce4799eeff2f4e8cd166809
Zach Toogood <zrtoogood@gmail.com>
2025-09-02 22:52:46 +0100

    Ready to clean

--- include/corororo/corororo.h
+++ include/corororo/corororo.h
@@ -1,15 +1,2 @@
 #pragma once
 
-//
-// CoroRoro New Implementation
-//
-//   A convenience header that includes the new CoroRoro implementation.
-//
-
-#include <corororo/coroutine/types.h>
-#include <corororo/coroutine/promise.h>
-#include <corororo/coroutine/task.h>
-
-#include <corororo/scheduler/scheduler.h>
-#include <corororo/scheduler/worker_pool.h>
-
